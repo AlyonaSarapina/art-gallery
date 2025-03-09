b25b4052426136f0e5e48996afd06488552c69f3
Alyona Sarapina <alyona.sarapina@gmail.com>
2025-03-09 23:07:27 +0000

    fixed api url

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,4 +1,3 @@
-const API_BASE_URL =
-  "https://art-gallery-backend-yme0.onrender.com/api/artworks";
+const API_BASE_URL = "https://art-gallery-backend-yme0.onrender.com/api";
 
 export default API_BASE_URL;
